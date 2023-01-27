f2adca038d49834176b78cc4b03cda2e7f9684a0
solomwaura <solomonmicro@gmail.com>
2023-01-27 13:39:27 +0300

    complete sorting algo

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React,{useState,useEffect} from 'react';
+import './Header.css';
+
+function Header() {
+
+  const [showHeaderBar, setShowHeaderBar] = useState(true);
+
+  const contactHandler = () => {
+    setShowHeaderBar(!showHeaderBar);
+  }
+
+  return (
+    <div className="header">
+      <div id="header__bar" >
+        {/* <h3 className="header__bar__title">Frontend Developer</h3> */}
+        <div className="header__bar__icons">
+
+        </div>
+      </div>
+
+      <div className="header-logo"><span>Artifact Algo Visualisation</span></div>
+      <div className="header__options">
+         
+      </div>
+   </div>
+  )
+}
+
+export default Header;
